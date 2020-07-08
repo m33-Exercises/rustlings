40ddfe922abc0fbdbec8a5391f69b73eff21d36c
Mustafa <mustafa.moiz125@gmail.com>
2020-07-08 12:15:12 -0700

    complete through macros

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,10 +7,17 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn ..... {
+fn calculate_apple_price(apples : i32) -> i32 {
+    let retail_price = 2;
+    let bulk_price = 1;
+
+    if apples <= 40 {
+        apples * retail_price
+    } else {
+        apples * bulk_price
+    }
+}
 
 // Don't modify this function!
 #[test]
